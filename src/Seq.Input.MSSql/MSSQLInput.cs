861908f407a2658755fb0ce08e14ecfbbfe9a6b6
Hinni Michael <Michael.Hinni@wincasa.ch>
2019-10-02 15:10:07 +0200

    Add solution and project

---
+++ src/Seq.Input.MSSql/MSSQLInput.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using Seq.Apps;
+
+namespace Seq.Input.MSSql
+{
+    [SeqApp("MSSQL Input", AllowReprocessing = false,
+        Description = "")]
+    public class MSSQLInput : SeqApp, IPublishJson
+    {
+        [SeqAppSetting(
+            DisplayName = "Refresh every x seconds",
+            IsOptional = false,
+            InputType = SettingInputType.Integer,
+            HelpText = "")]
+        public int QueryEverySeconds { get; set; } = 15;
+
+        [SeqAppSetting(
+            DisplayName = "Database Connection string",
+            IsOptional = false,
+            InputType = SettingInputType.Text,
+            HelpText = "")]
+        public string DatabaseConnectionString { get; set; }
+
+        [SeqAppSetting(
+            DisplayName = "Username",
+            IsOptional = false,
+            InputType = SettingInputType.Text,
+            HelpText = "")]
+        public string DatabaseUsername { get; set; }
+
+        [SeqAppSetting(
+            DisplayName = "Password",
+            IsOptional = false,
+            InputType = SettingInputType.Password,
+            HelpText = "")]
+        public string DatabasePassword { get; set; }
+
+        [SeqAppSetting(
+            DisplayName = "Query to execute on server",
+            IsOptional = false,
+            InputType = SettingInputType.Text,
+            HelpText = "")]
+        public string ExecuteQuery { get; set; }
+
+        public void Start(TextWriter inputWriter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
